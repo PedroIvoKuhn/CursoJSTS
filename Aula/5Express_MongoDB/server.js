ff99a72ba85552c227db04170565d5e945e28927
Pedro Ivo Kuhn <kuhnpedroivo@gmail.com>
2024-10-02 14:00:48 -0300

    Aula145-HelmeteCSRF

--- Aula/5Express_MongoDB/server.js
+++ Aula/5Express_MongoDB/server.js
@@ -1,22 +1,22 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-
 const mongoose = require('mongoose');
 mongoose.connect(process.env.CONNECTION_STRING)
     .then(()=> {
         app.emit('pronto');
     })
     .catch((e) => console.log(e));
-
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const flash = require('connect-flash');
-
 const routes = require('./routes');
 const path = require('path');
-const { middlewareGlobal } = require('./src/middlewares/middleware');
+const helmet = require('helmet');
+const csrf = require('csurf');
+const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
 
+app.use(helmet());
 // para conseguir ler o body da url
 app.use(express.urlencoded({ extended: true }));
 
@@ -38,8 +38,11 @@ app.use(flash());
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
+app.use(csrf());
 // Nossos próprios middlewares
 app.use(middlewareGlobal);
+app.use(checkCsrfError);
+app.use(csrfMiddleware);
 app.use(routes);
 
 app.on('pronto', () => {
